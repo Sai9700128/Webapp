789ee7393a15aca870e0e538a86942ba58664abe
Sai9700128 <burra.sa@northeastern.edu>
2025-01-29 12:06:27 -0500

    Added service layer to save the log details

---
+++ HealthCheck/src/main/java/com/Healthcheck/HealthCheck/Servicelayer/HealthCheckService.java
@@ -0,0 +1,24 @@
+package com.Healthcheck.HealthCheck.Servicelayer;
+
+import com.Healthcheck.HealthCheck.Entities.HealthCheck;
+import com.Healthcheck.HealthCheck.Repository.HealthCheckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HealthCheckService {
+
+    @Autowired
+    private HealthCheckRepository healthCheckRepository;
+
+    public boolean logHealthCheck() {
+        try {
+            HealthCheck healthCheck = new HealthCheck();
+            healthCheckRepository.save(healthCheck);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
+
