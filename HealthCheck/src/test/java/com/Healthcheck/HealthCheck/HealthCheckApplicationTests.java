61c3eef2e1392cb42a5f5c3d18e3daac7c8b9c82
Sai9700128 <burra.sa@northeastern.edu>
2025-02-05 15:07:30 -0500

    API testing displays success and failure

--- HealthCheck/src/test/java/com/Healthcheck/HealthCheck/HealthCheckApplicationTests.java
+++ HealthCheck/src/test/java/com/Healthcheck/HealthCheck/HealthCheckApplicationTests.java
@@ -1,13 +1,69 @@
 package com.Healthcheck.HealthCheck;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.given;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class HealthCheckApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8080;  // Change if running on a different port
+    }
 
+    @Test
+    void contextLoads() {
+        // Spring Boot Context Load Test
+    }
+
+    @Test
+    void testHealthCheckSuccess() {
+        given()
+                .when()
+                .get("/healthz")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void testInvalidEndpointReturns404() {
+        given()
+                .when()
+                .get("/invalid-endpoint")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void testRequestNotAllowed405() {
+        
+         // List of all methods to test except GET
+         String[] methods = {"POST", "PUT", "DELETE", "PATCH"};
+
+         for (String method : methods) {
+             given()
+                     .when()
+                     .request(method , "/healthz")  // Using dynamic method (POST, PUT, DELETE, PATCH, HEAD)
+                     .then()
+                     .statusCode(405);  // Should return 405 Method Not Allowed
+         }
+ 
+    }
+
+    @Test
+    void testGetWithPayloadReturns400() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"key\":\"value\"}")  // Sending a payload
+                .when()
+                .get("/healthz")
+                .then()
+                .statusCode(400);
+    }
 }
+
